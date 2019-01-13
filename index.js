a5a93710a113757f8587b584d94ed83f30c8efcf
Jeroen De Swaef <jeroen@faction.xyz>
2019-01-13 21:48:23 +0100

    Added logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
+    console.info('Got request')
 res.setHeader('Content-Type', 'text/plain');
 res.end('Hello World j\n');
 });
