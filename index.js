efa684c6f7d9b2d23902b233d65e54e4ebd6a49f
Jeroen De Swaef <jeroen@urbantz.com>
2019-01-27 18:56:36 +0100

    Printing length of secrets

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ app.use('/couch', proxy(process.env.COUCHDB_URL));
 const port = 3000
 
 app.get('/', (req, res) => res.send(`OK`))
+app.get('/info', (req, res) => res.send(`${process.env.COUCHDB_USER.length} - ${process.env.COUCHDB_PASSWORD.length}`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
